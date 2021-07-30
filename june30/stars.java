292cfb3e2a7bd476e5f27b8ae3194a26dd0351b9
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2021-07-30 23:05:53 +0100

    Create stars.java

---
+++ june30/stars.java
@@ -0,0 +1,33 @@
+package june30;
+
+public class stars {
+
+	public stars() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,j,row=6;
+
+for(i=0;i<row;i++)
+{
+	for(j=row-i;j>1;j--)
+	{
+		//for(j=0;j<row;j++)
+		System.out.print( "  "  );
+	}
+	for(j=0;j<=i;j++)
+	{
+	System.out.print(" * ");
+	
+}
+	//System.out.println(1);// one will add line end
+System.out.println();
+		
+		
+		
+	}
+
+}
+}
