e839754053c9e4d0b3fb74ce4534457853104e3e
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-04-25 15:45:45 +0100

    Java Interface
    Java Interface

---
+++ ABCEmployee.java
@@ -0,0 +1,32 @@
+
+package Interface;
+
+public class ABCEmployee implements EmployeeRules
+{
+public static void main(String[] args)
+{
+ABCEmployee abc = new ABCEmployee(); 
+abc.maintainHours(); 
+abc.relocate(); 
+}
+public void maintainHours()
+{
+System.out.println("40 Hours");
+}
+
+public void relocate()
+{
+System.out.println("Other City");
+}
+  
+public void report()
+{
+System.out.println("Manager");
+}
+  
+public void dress()
+{
+System.out.println("Neatly");
+}
+
+}
